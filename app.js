e3a3ddcec0d7464aec20aec75b02877735245324
Estifanose Sahilu <112593883+estif0@users.noreply.github.com>
2023-06-21 08:33:14 -0400

    Update app.js

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const hoursEl = document.getElementById('hours');
 const minsEl = document.getElementById('minutes');
 const secondsEl = document.getElementById('seconds');
 
-const final = "11 June 2023";
+const final = "9 July 2023";
 
 function countdown() {
     const finalDate = new Date(final);
